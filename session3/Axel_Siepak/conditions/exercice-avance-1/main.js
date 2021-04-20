a23df41da9a63d1d121e237368eb8fd7a970cd38
EtienneDx <etiennedx00@gmail.com>
2021-04-20 21:01:26 +0200

    debut session3

--- session3/Axel_Siepak/conditions/exercice-avance-1/main.js
+++ session3/Axel_Siepak/conditions/exercice-avance-1/main.js
@@ -1,4 +1,4 @@
-// ------ TODO 1 ------ 
+// ------ TODO 1 ------
 /* Exercice avancé : Change la couleur de fond du body */
 
 // Cette fonction est executée à chaque fois que l'utilisateur va changer
@@ -6,7 +6,6 @@
 // qu'il va "changer" d'option... c'est pourquoi elle est liée à l'événement onChange
 // dans le code HTML.
 function selectChange() {
-
   // cette variable récupère la valeur de l'option sélectionnée.
   var color = document.getElementById('colors').value;
 
@@ -20,12 +19,16 @@ function selectChange() {
   // Pour changer la couleur de fond du body, il faut utiliser le code ci-dessous:
   // document.body.style.backgroundColor = '...';
 
-
   /* MON CODE EN DESSOUS */
+  if (color == 'bleu') {
+    document.body.style.backgroundColor = 'blue';
+  } else if (color == 'rouge') {
+    document.body.style.backgroundColor = 'red';
+  } else {
+    document.body.style.backgroundColor = 'green';
+  }
+}
+/* MON CODE AU DESSUS */
 
-  /* MON CODE AU DESSUS */
-
-};
-
-// ------ TODO 2 ------ 
+// ------ TODO 2 ------
 // Complétez l'exercice avancé 2 situé dans le dossier "exercice-avance-2" un niveau au dessus
