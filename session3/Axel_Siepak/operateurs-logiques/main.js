91e8efb3c7c30677b5af2d49ae1439ab1e03809f
EtienneDx <etiennedx00@gmail.com>
2021-04-20 21:01:50 +0200

    debut session3 - fichiers pas sauvegardés

--- session3/Axel_Siepak/operateurs-logiques/main.js
+++ session3/Axel_Siepak/operateurs-logiques/main.js
@@ -6,40 +6,38 @@ console.log('Vous êtes dans les exercices sur les opérateurs logiques');
 // Écrire le code pour obtenir la valeur booléenne des cas suivants:
 // 1. faux ou faux
 
-
+false || false
 
 // 2. vrai et vrai
 
-
+true && true
 
 // 3. pas faux
 
-
+!true
 
 // 4. pas pas vrai
 
-
+!false
 
 // 5. pas pas faux
 
-
+!true
 
 
 // ------ TODO 2 ------ 
 // 1. Créer une variable var1 qui contient un booléen (true ou false).
 
-
-
+var var1 = true;
 
 // 2. Créer une variable var2 qui contient la valeur inverse de var1 (opérateur PAS).
 
-
-
+var var2 = false;
 
 // 3. Afficher dans la console l'opération logique var1 ET var2 et vérifier que le résultat
 // affiché est bien égal à false.
 
-
+console.log(var1 && var2); //false
 
 
 
@@ -47,21 +45,20 @@ console.log('Vous êtes dans les exercices sur les opérateurs logiques');
 // Écrire le code pour afficher les valeurs booléennes avec la fonction Boolean() pour:
 // 1. "a"
 
-
+console.log(Boolean("a"));  // true
 
 // 2. ""
 
-
+console.log(Boolean(""));   // false
 
 // 3. 1
 
-
+console.log(Boolean(1));    // true
 
 // 4. 0
 
-
+console.log(Boolean(0));    // false
 
 // 5. null
 
-
-
+console.log(Boolean(null)); // false
